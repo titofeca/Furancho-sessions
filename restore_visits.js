dc3f3686e24c36e444e5541a28281274ab76eaea
titofeca <83719382+titofeca@users.noreply.github.com>
2026-07-28 10:13:27 +0200

    ´´´bugas

--- restore_visits.js
+++
@@ -1,8 +0,0 @@
-const { db } = require('./db/database');
-const result = db.prepare(`
-  UPDATE sessions 
-  SET counted_as_visit = 1 
-  WHERE strftime('%w', entry_time) = '4' 
-    AND strftime('%H', entry_time) >= '16'
-`).run();
-console.log("Restored:", result.changes);
--- test_ranking.js
+++
@@ -1,4 +0,0 @@
-const { db, getCorchoRanking } = require('./db/database');
-const wallet = '0x3bdE3779DB08057A372b36577A999c34A268C54D';
-const rank = getCorchoRanking(wallet);
-console.log("Rank for", wallet, ":", rank);
